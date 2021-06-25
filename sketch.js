ae2b236dce65a951ec177d5e0551b87809af8c37
ClarissaDarla <85683142+ClarissaDarla@users.noreply.github.com>
2021-06-25 21:04:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var garden,rabbit;
+var gardenImg,rabbitImg;
+var apple;
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+}
+
+function setup(){
+  
+createCanvas(400,400);
+  
+// Moving background
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+//creating rabbit
+rabbit = createSprite(180,340,30,30);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+
+}
+
+function draw() {
+  background(0);
+  
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+
+  rabbit.x = World.mouseX;
+
+  createApples();
+
+  drawSprites();
+}
+
+function createApples(){
+ 
+ if (frameCount%80==0) {
+  
+  apple = createSprite(200,10,20,20);
+  apple.shapeColor = "red";
+  apple.velocityY = 3;
+
+  apple.y = Math.round(random(20,60));
+
+ }
+   
+}
